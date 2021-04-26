c047fc43bb9fca53a1b76a8e44eeac3bbe01419c
Jorge Borges <jdborges187@gmail.com>
2021-04-26 17:09:04 -0400

    login page and adds user

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css'
+
+const LoginFormPage = () => {
+    const dispatch = useDispatch();
+    const sessionUser = useSelector(state => state.session.user);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    if (sessionUser) return (
+        <Redirect to='/' />
+    )
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrors([]);
+        return dispatch(sessionActions.login({ email, password }))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) setErrors(data.errors);
+            });
+    }
+
+    return (
+        <div className="login-form-holder">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <ul>
+                    {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+                </ul>
+                <label>
+                    Email
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </label>
+                <label>
+                    Password
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </label>
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default LoginFormPage
