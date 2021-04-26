6c17518db6c82524b1ffb7d0fba84569662dd4b8
Jorge Borges <jdborges187@gmail.com>
2021-04-26 16:06:15 -0400

    added sessionReducer

---
+++ frontend/src/store/session.js
@@ -0,0 +1,67 @@
+import { csrfFetch } from "./csrf"
+
+// Constants
+const LOAD_USER = 'session/LOAD_USER'
+const REMOVE_USER = 'session/REMOVE_USER'
+
+
+const loadUser = user => ({
+    type: LOAD_USER,
+    user,
+})
+
+const removeUser = () => ({
+    type: REMOVE_USER,
+})
+
+export const login = (user) => async dispatch => {
+    const { email, password } = user;
+    const res = await csrfFetch('/api/session', {
+        method: 'POST',
+        body: JSON.stringify({
+            email,
+            password,
+        }),
+    })
+
+    if (!res.ok) throw res;
+
+    const data = await res.json();
+
+    dispatch(loadUser(data.user));
+
+    return res;
+}
+
+export const logout = () => async dispatch => {
+    const res = await csrfFetch('/api/session', {
+        method: 'DELETE'
+    })
+
+    if (!res.ok) throw res;
+
+    // const msg = await res.json();
+    // console.log(msg);
+
+    dispatch(removeUser())
+}
+
+
+const initialState = { user: null }
+
+const sessionReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case LOAD_USER: {
+            // console.log(action.user);
+            return { ...state, user: action.user };
+        }
+        case REMOVE_USER: {
+            const newState = { ...state };
+            newState.user = null;
+            return newState;
+        }
+        default: return state;
+    }
+}
+
+export default sessionReducer;
