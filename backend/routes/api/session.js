e79e98837caa5e89ce6276542686698658d1a006
Jorge Borges <jdborges187@gmail.com>
2021-04-25 23:03:19 -0400

    logins validating

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,42 +1,57 @@
 const express = require('express')
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+//* Some Validators for body of Post Request
+const validateLogin = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide an email")
+    .isEmail()
+    .withMessage("Please provide a valid email"),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a password.'),
+  handleValidationErrors,
+];
+
 //? Let me in now, LET ME IN NOWW!
-router.post('/', asyncHandler(async (req,res,next)=>{
-    const { email, password } = req.body;
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
 
-    const user = await User.login({ email, password });
+  const user = await User.login({ email, password });
 
-    if (!user) {
-      const err = new Error('Login failed');
-      err.status = 401;
-      err.title = 'Login failed';
-      err.errors = ['The provided credentials were invalid.'];
-      return next(err);
-    }
+  if (!user) {
+    const err = new Error('Login failed');
+    err.status = 401;
+    err.title = 'Login failed';
+    err.errors = ['The provided credentials were invalid.'];
+    return next(err);
+  }
 
-    await setTokenCookie(res, user);
+  await setTokenCookie(res, user);
 
-    return res.json({
-      user,
-    });
+  return res.json({
+    user,
+  });
 }))
 
 // Get outta here!
 router.delete(
-    '/',
-    (_req, res) => {
-      res.clearCookie('token');
-      return res.json({ message: 'success' });
-    }
-  );
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
 
 
-  // Restore session user
+// Restore session user
 router.get(
   '/',
   restoreUser,
