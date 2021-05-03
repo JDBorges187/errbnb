6417e1bf6645d0913595a25e27cb99d75ac31aa9
Jorge Borges <jdborges187@gmail.com>
2021-05-03 12:01:22 -0400

    updated booking seeder

--- backend/db/seeders/20210503072254-Bookings.js
+++ backend/db/seeders/20210503072254-Bookings.js
@@ -1,19 +1,39 @@
 'use strict';
 
+const { User, Place } = require('../models')
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
+  up: async (queryInterface, Sequelize) => {
     /*
       Add altering commands here.
       Return a promise to correctly handle asynchronicity.
 
       Example:
       */
-   return queryInterface.bulkInsert('Bookings', [{
-     userId: 1,
-     placeId: 20,
-     startDate: new Date('2021-05-13'),
-     endDate: new Date('2021-05-22')
-   }], {});
+
+      const demoUser = await User.findOne({
+        where: {
+          email: 'demo@user.io'
+        }
+      })
+
+      const places = await Place.findAll({
+        attributes: ['id']
+      })
+
+      let bookings =[]
+      if (!places) {
+        bookings = null;
+      } else {
+        bookings = [{
+          userId: demoUser.id,
+          placeId: places[0].id,
+          startDate: new Date('2021-05-13'),
+          endDate: new Date('2021-05-22')
+        }]
+      }
+
+   return queryInterface.bulkInsert('Bookings', bookings, {});
   },
 
   down: (queryInterface, Sequelize) => {
